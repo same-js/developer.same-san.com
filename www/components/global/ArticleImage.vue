5ba610548edc15a0c9e6fe3fb7bb5bd38d22bf48
same-js <40536011+same-js@users.noreply.github.com>
2021-12-26 06:56:28 +0900

    [Add] Component displaying image to article body

---
+++ www/components/global/ArticleImage.vue
@@ -0,0 +1,9 @@
+<template>
+  <img :src="'/images/article/' + src" style="width: 100%;">
+</template>
+<script>
+  export default {
+      // alt については受取はできる状態にしておくが、現時点では反映imgタグに反映しない（altテキストをどう表示するか、というデザインの実装をしてから）
+      props: ['src', 'alt']
+  }
+</script>
