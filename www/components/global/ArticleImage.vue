b889f72c1b8827a54580b9078a8349de5d1227db
same-js <40536011+same-js@users.noreply.github.com>
2021-12-26 07:21:11 +0900

    [Update] change the image in an article body to a link

--- www/components/global/ArticleImage.vue
+++ www/components/global/ArticleImage.vue
@@ -1,9 +1,19 @@
 <template>
-  <img :src="'/images/article/' + src" style="width: 100%;">
+  <a :href="imgPath">
+    <img :src="imgPath" style="width: 100%;">
+  </a>
 </template>
 <script>
   export default {
       // alt については受取はできる状態にしておくが、現時点では反映imgタグに反映しない（altテキストをどう表示するか、というデザインの実装をしてから）
-      props: ['src', 'alt']
+      props: ['src', 'alt'],
+      data () {
+        return {
+            imgPath: ''
+        }
+      },
+      mounted () {
+          this.imgPath = `/images/article/${this.src}`
+      }
   }
 </script>
